3df34e7cbc034d3f35190e507607ce12f7cc2539
Rimallasfcx111 <113599111+Rimallasfcx111@users.noreply.github.com>
2022-10-11 09:22:35 +0300

    JS nambers

---
+++ js/math.js
@@ -0,0 +1,15 @@
+
+// atvaizduoja daugiau klaidu.
+'use strict';
+console.log('math.js');
+
+let num1 = 11.458721
+
+// Math.round()
+
+let roundeded = Math.round(num1);
+console.log("rounded",roundeded);
+
+//floor
+//ceil
+//toFixed
